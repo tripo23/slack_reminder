46a5cc86a97af7813396a9ef98f2447016b05cb5
Nicolas Tripodi <tripo@Nicolass-Laptop.local>
2025-05-18 09:41:26 -0300

    uso fecha para comparar evento

--- slack_reminder_.py
+++ slack_reminder_.py
@@ -109,9 +109,9 @@ def programar_recordatorio(evento):
     print(f"Fecha del evento: {fecha_evento}")
 
     fecha_recordatorio = fecha_evento - timedelta(days=20)
-    print(f"fecha recordatorio: {fecha_recordatorio}, hoy es {datetime.now().date()}")
+    print(f"fecha recordatorio: {fecha_recordatorio.date()}, hoy es {datetime.now().date()}")
 
-    if (fecha_recordatorio == datetime.now().date()):
+    if (fecha_recordatorio.date() == datetime.now().date()):
         print(f"se va a enviar un recordatorio para {evento['nombre']}")
         enviar_mensaje(evento)
     
